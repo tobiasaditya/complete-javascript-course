1c84f0f0d426b817cdf62e5ef9865ce2ef7cc46f
Tobias <tobias.aditya@gmail.com>
2023-06-14 11:49:21 +0700

    do challenge 2

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -112,3 +112,51 @@ function printGoals(...nameScored) {
 printGoals('Davies', 'Muller', 'Lewandowski', 'Kimmich')
 
 console.log(team1 < draw && team1 < team2 && "team1 wins!" || draw < team1 && draw < team2 && "draw!" || team2 < draw && team2 < team1 && "team2 wins!")
+
+///////////////////////////////////////
+// Coding Challenge #2
+
+/* 
+Let's continue with our football betting app!
+
+1. Loop over the game.scored array and print each player name to the console, along with the goal number (Example: "Goal 1: Lewandowski")
+2. Use a loop to calculate the average odd and log it to the console (We already studied how to calculate averages, you can go check if you don't remember)
+3. Print the 3 odds to the console, but in a nice formatted way, exaclty like this:
+      Odd of victory Bayern Munich: 1.33
+      Odd of draw: 3.25
+      Odd of victory Borrussia Dortmund: 6.5
+Get the team names directly from the game object, don't hardcode them (except for "draw"). HINT: Note how the odds and the game objects have the same property names 😉
+
+BONUS: Create an object called 'scorers' which contains the names of the players who scored as properties, and the number of goals as the value. In this game, it will look like this:
+      {
+        Gnarby: 1,
+        Hummels: 1,
+        Lewandowski: 2
+      }
+
+GOOD LUCK 😀
+*/
+
+for (const [idx, p] of game.scored.entries()) {
+    console.log(`Goal ${idx + 1}: ${p}`)
+}
+
+let sumOdd = 0
+let gameOdds = Object.values(game.odds)
+for (const ov of gameOdds) {
+    sumOdd += ov
+}
+
+console.log(sumOdd / gameOdds.length)
+
+for (const o of Object.entries(game.odds)) {
+    console.log(`Odd of victory ${game[o[0]] ?? "draw"} : ${o[1]}`)
+}
+
+let scorers = {}
+
+for (const p of game.scored) {
+    scorers[p] = scorers[p] + 1 || 1
+}
+
+console.log(scorers)
