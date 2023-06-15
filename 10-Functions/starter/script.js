a1824f573102c9a7ea7fd791c7fda0cd54d9ca4d
Tobias <tobias.aditya@gmail.com>
2023-06-15 11:21:59 +0700

    do challenge 1

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,1 +1,118 @@
 'use strict';
+
+let bookings = []
+
+function createBooking(flightNum, numPassengers = 180, price = 1000) {
+    const booking = {
+        flightNum,
+        numPassengers,
+        price
+    }
+
+    console.log(booking)
+    bookings.push(booking)
+}
+
+createBooking("GA729")
+
+
+const flight = "GA123"
+const tobi = {
+    name: "Tobias",
+    passport: 1234562981
+}
+
+console.log(flight, tobi)
+function checkIn(flightNum, passenger) {
+    flightNum = "ID676"
+    passenger.name = "Mr. Tobias"
+
+    if (passenger.passport == 1234562981) {
+        alert("Checked in")
+    } else {
+        alert("Wrong data")
+    }
+}
+
+// checkIn(flight, tobi)
+
+console.log(flight, tobi)
+
+///////////////////////////////////////
+// Coding Challenge #1
+
+/* 
+Let's build a simple poll app!
+
+A poll has a question, an array of options from which people can choose, and an array with the number of replies for each option. This data is stored in the starter object below.
+
+Here are your tasks:
+
+1. Create a method called 'registerNewAnswer' on the 'poll' object. The method does 2 things:
+  1.1. Display a prompt window for the user to input the number of the selected option. The prompt should look like this:
+        What is your favourite programming language?
+        0: JavaScript
+        1: Python
+        2: Rust
+        3: C++
+        (Write option number)
+  
+  1.2. Based on the input number, update the answers array. For example, if the option is 3, increase the value AT POSITION 3 of the array by 1. Make sure to check if the input is a number and if the number makes sense (e.g answer 52 wouldn't make sense, right?)
+2. Call this method whenever the user clicks the "Answer poll" button.
+3. Create a method 'displayResults' which displays the poll results. The method takes a string as an input (called 'type'), which can be either 'string' or 'array'. If type is 'array', simply display the results array as it is, using console.log(). This should be the default option. If type is 'string', display a string like "Poll results are 13, 2, 4, 1". 
+4. Run the 'displayResults' method at the end of each 'registerNewAnswer' method call.
+
+HINT: Use many of the tools you learned about in this and the last section 😉
+
+BONUS: Use the 'displayResults' method to display the 2 arrays in the test data. Use both the 'array' and the 'string' option. Do NOT put the arrays in the poll object! So what shoud the this keyword look like in this situation?
+
+BONUS TEST DATA 1: [5, 2, 3]
+BONUS TEST DATA 2: [1, 5, 3, 9, 6, 1]
+
+GOOD LUCK 😀
+*/
+
+const poll = {
+    question: "What is your favourite programming language?",
+    options: ["0: JavaScript", "1: Python", "2: Rust", "3: C++"],
+    // This generates [0, 0, 0, 0]. More in the next section!
+    answers: new Array(4).fill(0),
+    registerNewAnswer() {
+        let strPrompt = this.question + "\n"
+        for (const option of this.options) {
+            strPrompt += option + "\n"
+        }
+        strPrompt += "(Write option number)"
+        console.log(strPrompt)
+        let answer = Number(prompt(strPrompt))
+
+        if (answer > 3 || answer < 0) {
+            alert("Invalid input")
+            this.registerNewAnswer()
+        } else {
+            this.answers[answer]++
+        }
+
+        this.displayResults("string")
+    },
+    displayResults(type = "array") {
+        if (type == "string") {
+            console.log(`Poll results are  ${this.answers.join(',')}`)
+            return
+        }
+        console.log(this.answers)
+
+    }
+};
+
+document.querySelector(".poll").addEventListener('click', poll.registerNewAnswer.bind(poll))
+
+let testData1 = {
+    answers: [5, 2, 3]
+}
+
+const generalDisplay = poll.displayResults
+generalDisplay.call(testData1)
+
+testData1.answers = [1, 5, 3, 9, 6, 1]
+generalDisplay.call(testData1, "string")
