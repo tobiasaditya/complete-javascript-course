0b13731c834c79ef8912a199c528da7e1de32038
Tobias <tobias.aditya@gmail.com>
2023-06-27 14:29:37 +0700

    add scrolling

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -36,63 +36,84 @@ document.addEventListener('keydown', function (e) {
 });
 
 
-///lecture
-//Select elements
-console.log(document.documentElement)
-console.log(document.head)
-console.log(document.body)
+// ///lecture
+// //Select elements
+// console.log(document.documentElement)
+// console.log(document.head)
+// console.log(document.body)
 
-const header = document.querySelector(".header")
-const allSections = document.querySelectorAll(".section")
+// const header = document.querySelector(".header")
+// const allSections = document.querySelectorAll(".section")
 
-console.log(allSections)
+// console.log(allSections)
 
-console.log(document.getElementById("section--1"))
-const allButtons = document.getElementsByTagName("button")
-console.log(allButtons)
+// console.log(document.getElementById("section--1"))
+// const allButtons = document.getElementsByTagName("button")
+// console.log(allButtons)
 
-console.log(document.getElementsByClassName("btn"))
+// console.log(document.getElementsByClassName("btn"))
 
-//Create and insert element
-// insertAdjacentHTML
+// //Create and insert element
+// // insertAdjacentHTML
 
-const message = document.createElement('div')
-message.classList.add("cookie-message")
-message.textContent = "We use cookies for I don't know what."
-message.innerHTML = "We use cookies for I don't know what. <button class='btn btn--close-cookie'> Got it! </button?>"
+// const message = document.createElement('div')
+// message.classList.add("cookie-message")
+// message.textContent = "We use cookies for I don't know what."
+// message.innerHTML = "We use cookies for I don't know what. <button class='btn btn--close-cookie'> Got it! </button?>"
 
-// header.prepend(message)
-// header.append(message.cloneNode(true))
-header.append(message)
+// // header.prepend(message)
+// // header.append(message.cloneNode(true))
+// header.append(message)
 
-header.before(message)
-header.after(message)
+// header.before(message)
+// header.after(message)
 
-//Delete elements
+// //Delete elements
 
-document.querySelector(".btn--close-cookie").addEventListener('click', (e) => {
-    message.remove()
+// document.querySelector(".btn--close-cookie").addEventListener('click', (e) => {
+//     message.remove()
+// })
+
+
+// //Styles
+// message.style.backgroundColor = "#37383d"
+// message.style.width = '120%'
+// console.log(message.style.width)
+
+// console.log(getComputedStyle(message).height)
+
+
+// document.documentElement.style.setProperty("--color-primary", "orangered")
+
+
+// //Attribute
+// const logo = document.querySelector(".nav__logo")
+// logo.alt = "Coba set alt logo"
+// logo.setAttribute('alt', "Dari method")
+// console.log(logo.getAttribute('alt'))
+// console.log(logo.getAttribute('class'))
+
+// //Data Attribute
+// console.log(logo.dataset)
+
+
+const buttonScrollTo = document.querySelector(".btn--scroll-to")
+const section1 = document.getElementById("section--1")
+
+buttonScrollTo.addEventListener('click', function (e) {
+    const s1coords = section1.getBoundingClientRect()
+    console.log(s1coords)
+    console.log(e.target.getBoundingClientRect())
+    console.log("Current Scroll ", scrollX, scrollY)
+
+    //Scrolling
+    //Old way
+    // window.scrollTo({
+    //     left: s1coords.left,
+    //     top: s1coords.top + scrollY, //ditambah Y biar jadi posisi absolut section1 ga peduli scrollnya
+    //     behavior: 'smooth'
+    // })
+
+    //Newer Way
+    section1.scrollIntoView({ behavior: 'smooth' })
 })
-
-
-//Styles
-message.style.backgroundColor = "#37383d"
-message.style.width = '120%'
-console.log(message.style.width)
-
-console.log(getComputedStyle(message).height)
-
-
-document.documentElement.style.setProperty("--color-primary", "orangered")
-
-
-//Attribute
-const logo = document.querySelector(".nav__logo")
-logo.alt = "Coba set alt logo"
-logo.setAttribute('alt', "Dari method")
-console.log(logo.getAttribute('alt'))
-console.log(logo.getAttribute('class'))
-
-//Data Attribute
-console.log(logo.dataset)
-
