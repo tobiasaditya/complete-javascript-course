7de2bd499c428139c6351954619a067ae86d8346
Tobias <tobias.aditya@gmail.com>
2023-07-03 15:08:21 +0700

    use leaflet for map

--- 15-Mapty/starter/script.js
+++ 15-Mapty/starter/script.js
@@ -15,6 +15,15 @@ navigator.geolocation.getCurrentPosition(function (position) {
     // console.log(position)
     const { latitude, longitude } = position.coords
     console.log(latitude, longitude)
+    const map = L.map('map').setView([latitude, longitude], 13);
+
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+
+    L.marker([latitude, longitude]).addTo(map)
+        .bindPopup('A pretty CSS popup.<br> Easily customizable.')
+        .openPopup();
 }, function () {
     alert("Couldn't get position")
 })
